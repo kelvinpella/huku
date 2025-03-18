427f17029eef8bfcc4a994144e0950a881baea36
kelvinpella <kelvinpella@gmail.com>
2025-03-18 03:01:42 +0300

    changed logo, footer and paragraph text size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full">
       <div className="w-full lg:max-w-screen-2xl mx-auto flex items-center justify-center py-6 border-t border-t-encore/20">
-         <div className="text-sm">© { new Date().getFullYear()} Huku kutoka TEYJA </div>
+         <div className="text-sm">© { new Date().getFullYear()} Huku | Mfumo kutoka TEJA </div>
       </div>
     </footer>
   );
