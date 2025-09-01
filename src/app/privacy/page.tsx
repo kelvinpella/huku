258fd74f59fd47dab3908baaa56b0728c40d523b
kelvinpella <kelvinpella@gmail.com>
2025-09-01 07:11:18 +0300

    Updated Privacy Policy and Terms of Service pages to reflect new titles and descriptions in English, enhancing clarity and accessibility. Adjusted footer links to match the updated page titles.

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,101 +1,105 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sera ya Faragha – hukutz.com",
+  title: "Privacy Policy – Huku",
   description:
-    "Sera rasmi ya Faragha ya hukutz.com. Soma kuhusu jinsi tunavyokusanya, kuhifadhi, na kutumia taarifa zako binafsi. Tunahakikisha usalama wa taarifa zako na tunaheshimu haki yako ya faragha.",
+    "The official Privacy Policy of Huku. Learn how we collect, store, and use your personal information. We are committed to safeguarding your data and respecting your right to privacy.",
 };
 
 export default function PrivacyPolicy() {
   return (
     <div className="w-full lg:max-w-screen-lg mx-auto px-2 py-6 my-12">
-      <h1>Sera ya Faragha</h1>
+      <h1>Privacy Policy</h1>
 
       <p>
-        <strong>hukutz.com</strong> tumejitolea kulinda na kuheshimu faragha
-        yako. Ikiwa una maswali kuhusu taarifa zako binafsi tafadhali wasiliana
-        nasi.
+        <strong>Huku</strong> is committed to protecting and respecting your
+        privacy. If you have any questions about your personal information,
+        please contact us.
       </p>
 
-      <h2>Taarifa Tunazokusanya Kuhusu Wewe</h2>
-      <p>Aina ya taarifa tunazokusanya ni pamoja na:</p>
+      <h2>Information We Collect About You</h2>
+      <p>The type of information we collect about you includes:</p>
       <ul className="list-disc list-inside">
-        <li>Jina lako au jina la mtumiaji</li>
-        <li>Anuani yako ya barua pepe (iliyotumika wakati wa usajili).</li>
-        <li>Tarehe na muda wa weka tangazo kwenye tovuti.</li>
-        <li>Maelezo kuhusu tangazo lako.</li>
-        <li>Taarifa nyingine utakazo toa ili kuwezesha kupata huduma hii</li>
-      </ul>
-
-      <p>Tunakusanya baadhi au taarifa zote hizi katika njia zifuatazo:</p>
-      <ul className="list-disc list-inside">
-        <li>Unapojisajili kama mwanachama wa tovuti hii.</li>
-        <li>Unapojaza fomu ya mawasiliano.</li>
-        <li>Unapotembelea tovuti hii.</li>
-        <li>Unapojaza sehemu katika wasifu wako.</li>
-      </ul>
-
-      <h2>Matumizi ya Taarifa Zako Binafsi</h2>
-      <p>Tunaweza kutumia taarifa zako binafsi kwa njia zifuatazo:</p>
-      <ul className="list-disc list-inside">
+        <li>Your name or username</li>
+        <li>Your email address (used when you sign up).</li>
+        <li>The date and time of your job post.</li>
+        <li>Information about your job.</li>
         <li>
-          Kutunza kumbukumbu za wanaotembelea ya tovuti kwa ajili ya usalama na
-          uthabiti wa mfumo wetu.
-        </li>
-        <li>
-          Kukuwezesha kuwa mwanachama aliyesajiliwa na kuchangia matangazo
-          kwenye tovuti hii.
-        </li>
-        <li>
-          Kukutumia barua pepe kukujulisha kuhusu shughuli mbalimbali kwenye
-          tovuti.
-        </li>
-        <li>
-          Kuwasiliana na wanachama wote mara kwa mara kwa matangazo muhimu
-          kupitia barua pepe.
+          Any other information you provide to enable you to use this service.
         </li>
       </ul>
 
       <p>
-        Tunaweza pia kukusanya taarifa ambazo hazikufanyi utambulike moja kwa
-        moja, kama vile aina ya kifaa unachotumia. Hii ni kwa madhumuni ya
-        uchambuzi na kufuatilia idadi ya watembeleaji wa tovuti yetu.
+        We may collect some or all of this information in the following ways:
       </p>
-
-      <h2>Ulinzi wa Taarifa Zako</h2>
-      <p>
-        hukutz.com imejizatiti kuhakikisha kuwa taarifa zote unazotupatia ziko
-        salama. Tumetekeleza hatua na taratibu stahiki za kuzuia uvujaji wa
-        taarifa binafsi.
-      </p>
-
-      <h2>Sera ya cookies</h2>
-      <p>
-        Cookies ni mafaili madogo ya maandishi tunayoweka kwenye kompyuta yako
-        ili kuwezesha huduma fulani, kama vile kuingia kwenye akaunti bila
-        kuweka nywila ( password ) mara kwa mara. Unaweza kusoma zaidi kuhusu
-        matumizi yetu ya cookies.
-      </p>
-
-      <h2>Haki Zako</h2>
-      <p>Una haki ya:</p>
       <ul className="list-disc list-inside">
-        {/* <li>Kuomba nakala ya taarifa binafsi tunazohifadhi kukuhusu.</li> */}
-        <li>Kurekebisha au kukamilisha taarifa zisizo sahihi au pungufu.</li>
-        <li>Kuomba tufute taarifa zako kabisa kutoka kwenye mfumo wetu.</li>
+        <li>You register as a user of this website.</li>
+        <li>You fill out a contact form.</li>
+        <li>You post a job.</li>
+        <li>You fill out a profile section.</li>
       </ul>
 
-      <h2>Uthibitisho wa Kukubali Sera</h2>
+      <h2>How We Use Your Personal Information</h2>
+      <p>We may use your personal information in the following ways:</p>
+      <ul className="list-disc list-inside">
+        <li>
+          Keeping records of website visitors for the security and stability of
+          our system.
+        </li>
+        <li>
+          Enabling you to become a registered member and contribute posts to
+          this website.
+        </li>
+        <li>
+          Using your email to send you important information about this website.
+        </li>
+        <li>
+          Communicating with all users regularly through important posts via
+          email.
+        </li>
+      </ul>
       <p>
-        Kuendelea kutumia tovuti yetu kunathibitisha kwamba umekubaliana na sera
-        hii. Ikiwa hukubaliani nayo, tafadhali usitumie tovuti hii.
+        We may also collect information that does not directly identify you,
+        such as the type of device you use. This is for the purpose of analytics
+        and tracking the number of visitors to our website.
       </p>
 
-      <h2>Mabadiliko ya Sera Hii</h2>
+      <h2>Protection of Your Information</h2>
       <p>
-        <strong>hukutz.com</strong> inaweza kufanya mabadiliko kwenye sera hii
-        wakati wowote. Unaweza kuombwa kupitia na kukubali upya sera hii ikiwa
-        kutakuwa na mabadiliko ya muhimu siku zijazo.
+        hukutz.com is committed to ensuring that all the information you provide
+        to us is secure. We have implemented appropriate measures and procedures
+        to prevent the leakage of personal information.
+      </p>
+
+      <h2>Cookie Policy</h2>
+      <p>
+        Cookies are small text files that we place on your computer to enable
+        certain services, such as logging into your account without having to
+        enter your password repeatedly. You can read more about our use of
+        cookies.
+      </p>
+
+      <h2>Your Rights</h2>
+      <p>You have the right to:</p>
+      <ul className="list-disc list-inside">
+        {/* <li>Request a copy of the personal information we hold about you.</li> */}
+        <li>Correct or complete inaccurate or incomplete information.</li>
+        <li>
+          Request that we delete your information entirely from our system.
+        </li>
+      </ul>
+
+      <h2>Acceptance of This Policy</h2>
+      <p>
+        Continuing to use our website confirms that you have agreed to this
+        policy. If you do not agree, please do not use this website.
+      </p>
+
+      <h2>Changes to This Policy</h2>
+      <p>
+        <strong>Huku</strong> may make changes to this policy at any time.
+        You may be asked to review and re-accept this policy if there are
+        significant changes in the future.
       </p>
     </div>
   );
